c05a97e3de9f5090aca0b5c3bd35fdacf6f1c66d
Deyvid Rannyere <deyran@gmail.com>
2021-04-29 11:29:06 -0300

    Solução estável
    Solução estável 01

--- Include/demos/autocomplete/celke/index.php
+++ Include/demos/autocomplete/celke/index.php
@@ -11,8 +11,22 @@
     <title>CELKE</title>
 </head>
 <body>
+    <h1>Pesquisar pessoa</h1>
+
+    <form>
+        <label>Nome da pessoa:</label>
+        <input type="text" name="descricao" id="descricao" placeholder="Digite o nome da pessoa" />
+    </form>
     
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>    
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+
+    <script type="text/javascript">
+        $(function() {
+
+        });
+
+    </script>
+    
 </body>
 </html>
