1ad923679e6027af6a51c0d87a76eb7e3df3a7e1
Deyvid Rannyere <deyran@gmail.com>
2021-04-29 14:40:34 -0300

    php e autocomplete
    14:40

--- Include/demos/autocomplete/celke/indexPesqPessoa.php
+++ Include/demos/autocomplete/celke/indexPesqPessoa.php
@@ -4,5 +4,17 @@ define('USER', 'root');
 define('PASS', '');
 define('DBNAME', 'eciv_docs_bd');
 
-$conn =new PDO("mysql:host=" . HOST . ";dbname=" . DBNAME . ";", USER, PASS);
+$CONNECTION =new PDO("mysql:host=" . HOST . ";dbname=" . DBNAME . ";", USER, PASS);
+
+$assunto = filter_input(INPUT_GET, 'term', FILTER_SANITIZE_STRING);
+
+$SQL_QUERY ="SELECT P.* FROM PESSOA AS P WHERE P.DESCRICAO = '%$assunto%' LIMIT 7";
+
+$RESULT = $CONNECTION->prepare($SQL_QUERY);
+$RESULT->execute();
+
+while($ROW = $RESULT)
+{
+
+}
 ?>
