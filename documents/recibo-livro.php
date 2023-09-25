54c997191df1155a007ae07ef26fcd4a32e21c6e
Deyvid Rannyere <deyran@gmail.com>
2023-09-25 14:04:00 -0300

    Primeiros testes em pdf

---
+++ documents/recibo-livro.php
@@ -0,0 +1,10 @@
+<?php 
+include("../Include/fpdf186/fpdf.php");
+
+$pdf = new FPDF('P', 'mm', "A4");
+$pdf->AddPage();
+$pdf->SetFont('Arial','B',16);
+$pdf->Cell(40,10,'Hello World!');
+$pdf->Output();
+
+?>
